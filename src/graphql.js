b314e0d89c1ac2b01aa4ddac52bd9cda5072a128
Samir <samir@lapilulerouge.io>
2020-07-31 16:23:29 +0200

    minor changes to enable graphql playground

--- src/graphql.js
+++ src/graphql.js
@@ -2,12 +2,17 @@ import { ApolloServer } from "apollo-server-lambda";
 import typeDefs from "./types/schema";
 import resolvers from "./types/resolvers";
 
-const server = new ApolloServer({ debug: false, typeDefs, resolvers });
+const server = new ApolloServer({
+  debug: false,
+  tracing: false,
+  typeDefs,
+  resolvers
+});
 
 const main = server.createHandler();
 
 exports.main = (event, context, callback) => {
-  function callbackFilter(error, output) {
+  const callbackFilter = (error, output) => {
     output.headers["Access-Control-Allow-Origin"] = "*";
     callback(error, output);
   }
